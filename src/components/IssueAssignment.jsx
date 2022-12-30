181435c538e1f3b7b353602d85162497ec225949
Andres Ausecha Mosquera <andres@aamsis.co>
2022-12-30 10:53:54 -0500

    add return to have better behavior in rollbacks

--- src/components/IssueAssignment.jsx
+++ src/components/IssueAssignment.jsx
@@ -11,7 +11,7 @@ export const IssueAssignment = ({ assignee, issueNumber }) => {
   const queryClient = useQueryClient();
   const setAssignment = useMutation(
     (assignee) => {
-      fetch(`/api/issues/${issueNumber}`, {
+      return fetch(`/api/issues/${issueNumber}`, {
         method: "PUT",
         headers: {
           "content-type": "application/json",
